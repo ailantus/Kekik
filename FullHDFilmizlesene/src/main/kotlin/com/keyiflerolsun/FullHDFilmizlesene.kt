efffdb83f1178ff503054380463516aa16725b0c
keyiflerolsun <keyiflerolsun@gmail.com>
2023-10-22 18:17:16 +0300

    🕊
    🕊

--- FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/FullHDFilmizlesene.kt
+++ FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/FullHDFilmizlesene.kt
@@ -121,7 +121,7 @@ class FullHDFilmizlesene : MainAPI() {
     private fun getRapidLink(document: Document): String? {
         Log.d("FHD", "document » $document")
 
-        val script   = document.selectXpath("(//script)[1]")?.text()?.trim() ?: return null
+        val script   = document.selectXpath("(//script)[1]")?.data()?.trim() ?: return null
         Log.d("FHD", "script » $script")
     
         val scx_pattern = "scx = (.*?);".toRegex()
