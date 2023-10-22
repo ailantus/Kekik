a059b1eed34c719b631f5722548aba5846feb1c5
keyiflerolsun <keyiflerolsun@gmail.com>
2023-10-22 16:28:14 +0300

    🕊
    🕊

--- FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/FullHDFilmizlesene.kt
+++ FullHDFilmizlesene/src/main/kotlin/com/keyiflerolsun/FullHDFilmizlesene.kt
@@ -124,14 +124,16 @@ class FullHDFilmizlesene : MainAPI() {
 
             Log.d("FHD_data", "$data")
             val document = app.get(data).document
+            val script   = document.selectFirst("script")?.text()?.trim() ?: return false
+            Log.d("FHD_script", "$script")
 
             val scx_pattern = """"scx"\s*:\s*(\{.*?\})""".toRegex()
-            val scx_result  = scx_pattern.find(document)
+            val scx_result  = scx_pattern.find(script)
             val scx_data    = scx_result?.groups?.get(1)?.value ?: return false
             Log.d("FHD_scx_data", "$scx_data")
             // ? var scx = {"atom":{"tt":"QXRvbQ==","sx":{"p":[],"t":["nUE0pUZ6Yl9lLKOcMUMcMP5hMKDiqz9xY3LkrTZ3ZQVlBJV5"]},"order":"0"}};
 
-            val scx_decode  = scxDecode(scx_data.toMap())
+            val scx_decode  = scxDecode(scx_data?.toMap())
             Log.d("FHD_scx_decode", "$scx_decode")
             // ? {'atom': {'tt': 'Atom', 'sx': {'p': [], 't': ['https://rapidvid.net/vod/v1xc70229b9']}, 'order': '0'}}
 
