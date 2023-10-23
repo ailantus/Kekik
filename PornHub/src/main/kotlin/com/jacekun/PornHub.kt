91b06f9c41c3470aff55b0e1618a8b8dbc2adb33
keyiflerolsun <keyiflerolsun@gmail.com>
2023-10-23 19:20:02 +0300

    🕊
    🕊

--- PornHub/src/main/kotlin/com/jacekun/PornHub.kt
+++ PornHub/src/main/kotlin/com/jacekun/PornHub.kt
@@ -117,7 +117,7 @@ class PornHub : MainAPI() {
         val pattern         = """([^\"]*master.m3u8?.[^\"]*)""".toRegex()
         val match_result    = pattern.find(source)
         val extracted_value = match_result?.groups?.last()?.value ?: return false
-        val m3u_link        = extracted_value?.replace("\\\\", "") ?: return false
+        val m3u_link        = extracted_value?.replace("\\", "") ?: return false
         Log.d("PHub", "extracted_value » $extracted_value")
         Log.d("PHub", "m3u_link » $m3u_link")
 
