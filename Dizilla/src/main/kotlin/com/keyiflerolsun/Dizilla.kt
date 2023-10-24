9e3d1cfbd7ffc592ce005eeb4851f48adbf0de2f
keyiflerolsun <keyiflerolsun@gmail.com>
2023-10-24 12:23:40 +0300

    🕊
    🕊

--- Dizilla/src/main/kotlin/com/keyiflerolsun/Dizilla.kt
+++ Dizilla/src/main/kotlin/com/keyiflerolsun/Dizilla.kt
@@ -21,7 +21,7 @@ class Dizilla : MainAPI() {
 
     override val mainPage           =
         mainPageOf(
-            "$mainUrl/altyazili-bolumler?page=" to "Altyazılı Bölümler",
+            "$mainUrl/trend/" to "Bu Ay Öne Çıkanlar",
         )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
@@ -33,8 +33,8 @@ class Dizilla : MainAPI() {
 
     private fun Element.toSearchResult(): SearchResponse? {
         val title     = this.selectFirst("h2")?.text() ?: return null
-        val href      = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
-        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("data-srcset")?.split(" ")?.first())
+        val href      = fixUrlNull(this.attr("href")) ?: return null
+        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
 
         return newTvSeriesSearchResponse(title, href, TvType.TvSeries) { this.posterUrl = posterUrl }
     }
@@ -42,19 +42,39 @@ class Dizilla : MainAPI() {
     override suspend fun load(url: String): LoadResponse? {
         val document = app.get(url).document
 
+        val title       = document.selectFirst("div.w-full h1")?.text() ?: return null
+        val poster      = fixUrlNull(document.selectFirst("div.w-full img")?.attr("src")) ?: return null
+        val year        = document.select("div.w-full span.text-cf")?.get(0)?.trim()?.toIntOrNull()
+        val description = document.selectFirst("div.left-content-paragraf")?.text()?.trim()
+
+        val episodes    = document.select("div.season-lists div.cursor-pointer").mapNotNull {
+            val href        = fixUrlNull(it.selectFirst("a.opacity-60")?.attr("href"))
+            val name        = it.select("a")?.last()?.text()?.trim()
+            val description = it.selectFirst("span.t-content")?.text()?.trim()
+            val episode     = it.selectFirst("a.opacity-60")?.text().toIntOrNull()
+
+            val parent_div   = cursorPointer.parent()
+            val season_class = parent_div?.className()?.split(" ")?.find { it.startsWith("szn") }
+            val season       = season_class?.substringAfter("szn")?.toIntOrNull()
+
+            Episode(href, name, season, episode) {
+                this.description = description
+            }
+        }
+
         return null
 
-        // return newMovieLoadResponse(title, url, TvType.Movie, url) {
-        //     this.posterUrl       = poster
-        //     this.year            = year
-        //     this.plot            = description
+        return newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
+            this.posterUrl       = poster
+            this.year            = year
+            this.plot            = description
         //     this.tags            = tags
         //     this.rating          = rating
         //     this.duration        = duration
         //     this.recommendations = recommendations
         //     addActors(actors)
         //     addTrailer(trailer)
-        // }
+        }
     }
 
     override suspend fun loadLinks(
