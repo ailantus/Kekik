a82d66815c5864ba495077c08664cbd072e188f2
keyiflerolsun <keyiflerolsun@gmail.com>
2023-10-25 00:33:28 +0300

    🕊
    🕊

---
+++ DiziBox/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 0
+
+cloudstream {
+    authors     = listOf("keyiflerolsun")
+    language    = "tr"
+    description = "YAPIM AŞAMASINDA!"
+    // description = "Yabancı Dizi izle, Tüm yabancı dizilerin yeni ve eski sezonlarını full hd izleyebileceğiniz elit site."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 3 // will be 3 if unspecified
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.dizibox.tv&sz=%size%"
+}
---
+++ DiziBox/src/main/kotlin/com/keyiflerolsun/DiziBox.kt
@@ -0,0 +1,59 @@
+// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+package com.keyiflerolsun
+
+import android.util.Log
+import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.ExtractorLink
+
+
+class DiziBox : MainAPI() {
+    override var mainUrl            = "https://www.dizibox.tv"
+    override var name               = "DiziBox"
+    override val hasMainPage        = true
+    override var lang               = "tr"
+    override val hasDownloadSupport = true
+    override val supportedTypes     = setOf(TvType.TvSeries)
+
+    override val mainPage =
+        mainPageOf(
+            "/?tur[0]=aile&yil&imdb&orderby=imdb"     to "Aile",
+            "/?tur[0]=aksiyon&yil&imdb&orderby=imdb"  to "Aksiyon",
+        )
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val document = app.get("$mainUrl/dizi-arsivi/page/" + page + request.data, referer="$mainUrl/").document
+        val home     = document.select("article.detailed-article").mapNotNull { it.toSearchResult() }
+
+        return newHomePageResponse(request.name, home)
+    }
+
+    private fun Element.toSearchResult(): SearchResponse? {
+        val title     = this.selectFirst("h3 a")?.text() ?: return null
+        val href      = fixUrlNull(this.selectFirst("h3 a")?.attr("href")) ?: return null
+        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
+
+        return newTvSeriesSearchResponse(title, href, TvType.TvSeries) { this.posterUrl = posterUrl }
+    }
+
+    override suspend fun load(url: String): LoadResponse? {
+        val document = app.get(url).document
+        Log.d("DZB", "url » $url")
+        Log.d("DZB", "document » $document")
+
+        return null
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+        ): Boolean {
+
+            Log.d("DZB", "data » $data")
+
+            return true
+    }
+}
