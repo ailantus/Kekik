93d6c61487230ddb39f55e5911b3d5cde4c57059
keyiflerolsun <keyiflerolsun@gmail.com>
2023-10-26 14:12:02 +0300

    🕊
    🕊

--- DiziPal/build.gradle.kts
+++ DiziPal/build.gradle.kts
@@ -1,10 +1,9 @@
-version = 0
+version = 1
 
 cloudstream {
     authors     = listOf("keyiflerolsun")
     language    = "tr"
-    description = "YAPIM AŞAMASINDA!"
-    // description = "dizipal güncel, dizipal yeni ve gerçek adresi. dizipal en yeni dizi ve filmleri güvenli ve hızlı şekilde sunar."
+    description = "en yeni dizileri güvenli ve hızlı şekilde sunar."
 
     /**
      * Status int as the following:
@@ -13,7 +12,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 3 // will be 3 if unspecified
+    status  = 1 // will be 3 if unspecified
     tvTypes = listOf("TvSeries")
     iconUrl = "https://www.google.com/s2/favicons?domain=dizipal625.com&sz=%size%"
 }
