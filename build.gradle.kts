538a21e06d0654b53cb27793d40375eae2c54464
keyiflerolsun <keyiflerolsun@gmail.com>
2023-10-26 13:38:22 +0300

    🕊
    🕊

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,7 +79,7 @@ subprojects {
         // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
         implementation(kotlin("stdlib"))                                              // Kotlin'in temel kütüphanesi
         implementation("com.github.Blatzar:NiceHttp:0.4.4")                           // HTTP kütüphanesi
-        implementation("org.jsoup:jsoup:1.15.3")                                      // HTML ayrıştırıcı
+        implementation("org.jsoup:jsoup:1.16.2")                                      // HTML ayrıştırıcı
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")   // Kotlin için Jackson JSON kütüphanesi
         implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")          // JSON-nesne dönüştürme kütüphanesi
     }
