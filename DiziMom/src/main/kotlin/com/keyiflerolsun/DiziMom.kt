82a099a7641ab75bda1f9530274c93c35b7a2798
keyiflerolsun <keyiflerolsun@gmail.com>
2023-10-28 11:27:57 +0300

    🕊
    🕊

--- DiziMom/src/main/kotlin/com/keyiflerolsun/DiziMom.kt
+++ DiziMom/src/main/kotlin/com/keyiflerolsun/DiziMom.kt
@@ -102,10 +102,10 @@ class DiziMom : MainAPI() {
         callback: (ExtractorLink) -> Unit
         ): Boolean {
 
-            Log.d("DZM", "data » $data")
+            Log.d("DZM", "_data » $data")
             val document = app.get(data).document
             val iframe   = document.selectFirst("div#vast iframe")?.attr("src") ?: return false
-            Log.d("DZM", "iframe » $iframe")
+            Log.d("DZM", "_iframe » $iframe")
 
 
             var i_source: String? = null
@@ -119,7 +119,7 @@ class DiziMom : MainAPI() {
             if (iframe.contains("hdplayersystem")) {
                 val vid_id   = iframe.substringAfter("video/")
                 val post_url = "https://hdplayersystem.live/player/index.php?data=${vid_id}&do=getVideo"
-                Log.d("DZM", "post_url » $post_url")
+                Log.d("DZM", "_post_url » $post_url")
 
                 val response = app.post(
                     post_url,
@@ -135,12 +135,13 @@ class DiziMom : MainAPI() {
                 )
                 i_source = response.toString()
 
-                m3u_link = response.parsedSafe<VideoResponse>()?.videoSources?.get(-1)?.file
+                val vid_extract = response.parsedSafe<VideoResponse>()?.videoSources?.get(-1)?.file
+                m3u_link        = vid_extract.replace("\\", "")
             }
 
             if (iframe.contains("peacemakerst") || iframe.contains("hdstreamable")) {
                 val post_url = "${iframe}?do=getVideo"
-                Log.d("DZM", "post_url » $post_url")
+                Log.d("DZM", "_post_url » $post_url")
 
                 val response = app.post(
                     post_url,
@@ -157,12 +158,13 @@ class DiziMom : MainAPI() {
                 )
                 i_source = response.toString()
 
-                m3u_link = response.parsedSafe<VideoResponse>()?.videoSources?.get(-1)?.file
+                val vid_extract = response.parsedSafe<VideoResponse>()?.videoSources?.get(-1)?.file
+                m3u_link        = vid_extract.replace("\\", "")
             }
 
-            Log.d("DZM", "m3u_link » $m3u_link")
+            Log.d("DZM", "_m3u_link » $m3u_link")
             if (m3u_link == null) {
-                Log.d("DZM", "i_source » $i_source")
+                Log.d("DZM", "_i_source » $i_source")
                 return false
             }
 
