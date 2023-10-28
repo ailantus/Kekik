346e6499ab25167842938200e37b3b0edb56e209
keyiflerolsun <keyiflerolsun@gmail.com>
2023-10-28 12:50:00 +0300

    🕊
    🕊

--- DiziMom/build.gradle.kts
+++ DiziMom/build.gradle.kts
@@ -12,7 +12,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 3 // will be 3 if unspecified
+    status  = 1 // will be 3 if unspecified
     tvTypes = listOf("TvSeries")
     iconUrl = "https://www.google.com/s2/favicons?domain=www.dizimom.pro&sz=%size%"
 }
