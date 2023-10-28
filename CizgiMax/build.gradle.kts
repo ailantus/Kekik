26de4ce8f4943ac82a114c9f90ab145493bdc401
keyiflerolsun <keyiflerolsun@gmail.com>
2023-10-28 17:56:50 +0300

    🕊
    🕊

---
+++ CizgiMax/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 0
+
+cloudstream {
+    authors     = listOf("keyiflerolsun")
+    language    = "tr"
+    description = "YAPIM AŞAMASINDA!"
+    // description = "ÇizgiMax ile Çizgi Film izlemek artık daha kolay, donmadan full hd ve reklamsız bir sitedir, içerisinde 700 den fazla çizgi film olan, Bu site bu işi profesyonelce yapıyor."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 3 // will be 3 if unspecified
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=cizgimax.online&sz=%size%"
+}
---
+++ CizgiMax/src/main/kotlin/com/keyiflerolsun/CizgiMax.kt
@@ -0,0 +1,115 @@
+// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+package com.keyiflerolsun
+
+import android.util.Log
+import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.Qualities
+
+
+class CizgiMax : MainAPI() {
+    override var mainUrl            = "https://cizgimax.online"
+    override var name               = "CizgiMax"
+    override val hasMainPage        = true
+    override var lang               = "tr"
+    override val hasQuickSearch     = false
+    override val hasDownloadSupport = true
+    override val supportedTypes     = setOf(TvType.TvSeries)
+
+    override val mainPage =
+        mainPageOf(
+            "$mainUrl/category/genel/aile/page/"        to "Aile",
+            "$mainUrl/category/genel/aksyion/page/"     to "Aksyion",
+            "$mainUrl/category/genel/bilim-kurgu/page/" to "Bilim Kurgu",
+            "$mainUrl/category/genel/fantastik/page/"   to "Fantastik",
+            "$mainUrl/category/genel/komedi/page/"      to "Komedi",
+            "$mainUrl/category/genel/korku/page/"       to "Korku",
+            "$mainUrl/category/genel/macera/page/"      to "Macera",
+            "$mainUrl/category/genel/tarih/page/"       to "Tarih"
+        )
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val document = app.get(request.data + page + "/?sort=views").document
+        val home     = document.select("div.movie-preview-content").mapNotNull { it.toSearchResult() }
+
+        return newHomePageResponse(request.name, home)
+    }
+
+    private fun Element.toSearchResult(): SearchResponse? {
+        val title     = this.selectFirst("span.movie-title")?.text()?.substringBefore(" Türkçe İzle") ?: return null
+        val href      = fixUrlNull(this.selectFirst("span.movie-title a")?.attr("href")) ?: return null
+        val posterUrl = fixUrlNull(this.selectFirst("span.center-icons img")?.attr("src"))
+
+        return newTvSeriesSearchResponse(title, href, TvType.TvSeries) { this.posterUrl = posterUrl }
+    }
+
+    override suspend fun quickSearch(query: String): List<SearchResponse> = search(query)
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val document = app.get("$mainUrl/?s=$query").document
+
+        return document.select("div.movie-preview-content").mapNotNull { it.toSearchResult() }
+    }
+
+    override suspend fun load(url: String): LoadResponse? {
+        val document = app.get(url).document
+
+        val title       = document.selectFirst("div.title h1")?.text() ?: return null
+        val poster      = fixUrlNull(document.selectFirst("div.poster img")?.attr("src")) ?: return null
+        val description = document.selectFirst("div.excerpt")?.text()?.trim()
+        val tags        = document.select("div.categories a").mapNotNull { it?.text()?.trim() }
+        val rating      = document.selectFirst("span.imdb-rating")?.text()?.substringBefore("IMDB Puanı")?.trim()?.toRatingInt()
+
+
+        Episode(
+            data        = url,
+            name        = "BÖLÜM 1",
+            season      = 1,
+            episode     = 1,
+            posterUrl   = null,
+            rating      = null,
+            date        = null
+        )
+        val episodes    = document.select("a.post-page-numbers").mapNotNull {
+            val ep_name    = it.selectFirst("div.part-name")?.text()?.trim() ?: return@mapNotNull null
+            val ep_href    = fixUrlNull(it.attr("href")) ?: return@mapNotNull null
+            val ep_episode = Regex("""BÖLÜM (\d+)""").find(ep_name)?.groupValues?.get(1)?.toIntOrNull()
+            val ep_season  = Regex("""S(\d+) BÖLÜM""").find(ep_name)?.groupValues?.get(1)?.toIntOrNull() ?: 1
+
+            Episode(
+                data        = ep_href,
+                name        = ep_name,
+                season      = ep_season,
+                episode     = ep_episode,
+                posterUrl   = null,
+                rating      = null,
+                date        = null
+            )
+        }
+
+        return newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
+            this.posterUrl = poster
+            this.plot      = description
+            this.tags      = tags
+            this.rating    = rating
+        }
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+        ): Boolean {
+
+            Log.d("CizgiMax", "data » $data")
+            val document = app.get(data).document
+            val iframe   = document.selectFirst(".series-player-container iframe")?.attr("src") ?: return false
+            Log.d("CizgiMax", "iframe » $iframe")
+            return false
+
+            // return true
+    }
+}
