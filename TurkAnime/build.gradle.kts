f8f5ad23111ae83fde3e8b9a269a53f8a37258ca
keyiflerolsun <keyiflerolsun@gmail.com>
2023-10-30 11:19:13 +0300

    🕊
    🕊

---
+++ TurkAnime/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 0
+
+cloudstream {
+    authors     = listOf("keyiflerolsun")
+    language    = "tr"
+    description = "YAPIM AŞAMASINDA!"
+    // description = "AnimeciX, animeleri keşfedebileceğiniz ve animeler hakkında her şeyi öğrenebileceğiniz bir paylaşım platformudur."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 3 // will be 3 if unspecified
+    tvTypes = listOf("Anime")
+    iconUrl = "https://www.google.com/s2/favicons?domain=animecix.net&sz=%size%"
+}
---
+++ TurkAnime/src/main/kotlin/com/keyiflerolsun/AnimeciX.kt
@@ -0,0 +1,103 @@
+// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+package com.keyiflerolsun
+
+import android.util.Log
+import org.jsoup.nodes.Element
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.Qualities
+
+class AnimeciX : MainAPI() {
+    override var mainUrl            = "https://animecix.net"
+    override var name               = "AnimeciX"
+    override val hasMainPage        = true
+    override var lang               = "tr"
+    override val hasQuickSearch     = false
+    override val hasDownloadSupport = true
+    override val supportedTypes     = setOf(TvType.Anime)
+
+    override val mainPage =
+        mainPageOf(
+            "$mainUrl/secure/titles?genre=drama&onlyStreamable=true" to "Aksiyon",
+        )
+
+    data class Category(
+        @JsonProperty("pagination") val pagination: Pagination,
+    )
+
+    data class Search(
+        @JsonProperty("results") val results: List<Anime>,
+    )
+
+    data class Pagination(
+        @JsonProperty("current_page") val current_page: Int,
+        @JsonProperty("last_page") val last_page: Int,
+        @JsonProperty("per_page") val per_page: Int,
+        @JsonProperty("data") val data: List<Anime>,
+        @JsonProperty("total") val total: Int,
+    )
+
+    data class Anime(
+        @JsonProperty("id") val id: Int,
+        @JsonProperty("name") val name: String,
+        @JsonProperty("description") val description: String,
+        @JsonProperty("poster") val poster: String,
+        @JsonProperty("mal_vote_average") val rate: Float?,
+        @JsonProperty("genres") val genres: List<Genre>,
+    )
+
+    data class Genre(
+        @JsonProperty("display_name") val name: String,
+    )
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val response = app.get(request.data + "&page=${page}&perPage=3")
+
+        val home     = response.parsedSafe<Category>().pagination.data.mapNotNull {
+            newTvSeriesSearchResponse(
+                this.name,
+                "$mainUrl/secure/titles/${this.id}",
+                TvType.Anime
+            ) {
+                this.posterUrl = this.poster
+            }
+        }
+
+        return newHomePageResponse(request.name, home)
+    }
+
+    override suspend fun quickSearch(query: String): List<SearchResponse> = search(query)
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val response = app.get("$mainUrl/secure/search/$query?limit=20")
+
+        return response.parsedSafe<Search>().results.mapNotNull {
+            newTvSeriesSearchResponse(
+                this.name,
+                "$mainUrl/secure/titles/${this.id}",
+                TvType.Anime
+            ) {
+                this.posterUrl = this.poster
+            }
+        }
+    }
+
+    override suspend fun load(url: String): LoadResponse? {
+        return null
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+        ): Boolean {
+
+            return false
+    }
+}
