ea6c11684f4655ebd36f6f128d1c8c7c42fbd19c
keyiflerolsun <keyiflerolsun@gmail.com>
2023-10-30 15:53:32 +0300

    🕊
    🕊

--- AnimeciX/src/main/kotlin/com/keyiflerolsun/AnimeciX.kt
+++ AnimeciX/src/main/kotlin/com/keyiflerolsun/AnimeciX.kt
@@ -186,7 +186,7 @@ class AnimeciX : MainAPI() {
                             url     = video.url,
                             referer = "$mainUrl/",
                             quality = Qualities.Unknown.value,
-                            isM3u8  = m3u_link.contains(".m3u8")
+                            isM3u8  = video.url.contains(".m3u8")
                         )
                     )
                 }
