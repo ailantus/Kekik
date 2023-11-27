898cdf85e1e1dfa4a09ab693d967a21d6334b550
keyiflerolsun <keyiflerolsun@gmail.com>
2023-11-27 10:55:45 +0300

    🕊
    🕊

--- DiziMom/src/main/kotlin/com/keyiflerolsun/DiziMom.kt
+++ DiziMom/src/main/kotlin/com/keyiflerolsun/DiziMom.kt
@@ -94,10 +94,10 @@ class DiziMom : MainAPI() {
         Log.d("DZM", "data » ${data}")
         val document = app.get(
             data,
-            headers = mapOf(
-                "User-Agent" to "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36",
-                "Cookie"     to "wordpress_logged_in_94427965a200eb7dd292509ed2c7c018=keyiflerolsun|1701195846|47kYwoaxbzwb7LQn3M5P4x2nBB2DRp8ky2tuHYYGffr|b504980f0c14ec2b85bc788ccfe903c8aeee0387e739ad0a42297b0ee874b406"
-            )
+            // headers = mapOf(
+            //     "User-Agent" to "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36",
+            //     "Cookie"     to "wordpress_logged_in_94427965a200eb7dd292509ed2c7c018=keyiflerolsun|1701195846|47kYwoaxbzwb7LQn3M5P4x2nBB2DRp8ky2tuHYYGffr|b504980f0c14ec2b85bc788ccfe903c8aeee0387e739ad0a42297b0ee874b406"
+            // )
         ).document
         val iframe   = document.selectFirst("div#vast iframe")?.attr("src") ?: return false
         Log.d("DZM", "iframe » ${iframe}")
