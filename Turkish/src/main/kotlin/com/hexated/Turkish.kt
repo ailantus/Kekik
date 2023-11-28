638353988aad0894da93ec9cd1ca16eab2e0b324
Furkan Ertürk <138170751+sxwcxz@users.noreply.github.com>
2023-11-28 11:50:14 +0300

    Update Turkish.kt

--- Turkish/src/main/kotlin/com/hexated/Turkish.kt
+++ Turkish/src/main/kotlin/com/hexated/Turkish.kt
@@ -8,8 +8,8 @@ import com.lagradost.cloudstream3.utils.loadExtractor
 import org.jsoup.nodes.Element
 
 class Turkish : MainAPI() {
-    override var mainUrl = "https://turkish123.com"
-    override var name = "Turkish123"
+    override var mainUrl = "https://turkish123.today"
+    override var name = "TurkceTV"
     override val hasMainPage = true
     override var lang = "tr"
     override val hasDownloadSupport = true
