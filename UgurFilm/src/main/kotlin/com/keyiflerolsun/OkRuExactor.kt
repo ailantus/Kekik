56df56c66216d7a598b9142f43108fd0e02ef4ed
keyiflerolsun <keyiflerolsun@gmail.com>
2023-12-10 18:19:58 +0300

    🕊
    🕊

--- UgurFilm/src/main/kotlin/com/keyiflerolsun/OkRuExactor.kt
+++ UgurFilm/src/main/kotlin/com/keyiflerolsun/OkRuExactor.kt
@@ -21,10 +21,7 @@ open class OkRu : ExtractorApi() {
                 Integer.parseInt(matchResult.groupValues[1], 16).toChar().toString()
             }
         val videos_str = Regex("""\"videos\":(\[[^\]]*\])""").find(video_req)?.groupValues?.get(1) ?: throw ErrorLoadingException("Video not found")
-        Log.d("Kekik_${this.name}", "videos_str » ${videos_str}")
-
         val videos = AppUtils.tryParseJson<List<OkRuVideo>>(videos_str) ?: throw ErrorLoadingException("Video not found")
-        Log.d("Kekik_${this.name}", "videos » ${videos}")
 
         for (video in videos) {
             Log.d("Kekik_${this.name}", "video » ${video}")
