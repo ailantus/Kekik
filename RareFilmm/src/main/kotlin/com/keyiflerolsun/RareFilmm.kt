38191f3ccb8bb72db8d1f4b15b05ae24f353acad
keyiflerolsun <keyiflerolsun@gmail.com>
2023-12-15 23:42:58 +0300

    🕊
    🕊

--- RareFilmm/src/main/kotlin/com/keyiflerolsun/RareFilmm.kt
+++ RareFilmm/src/main/kotlin/com/keyiflerolsun/RareFilmm.kt
@@ -63,7 +63,7 @@ class RareFilmm : MainAPI() {
         val poster      = fixUrlNull(document.selectFirst("div.featured-image")?.attr("style")?.substringAfter("url(")?.substringBefore(")"))
         val description = document.selectFirst("div.entry-content p")?.text()?.trim()
         val tags        = document.select("div.entry-tags a").map { it.text() }
-        val rating      = document.selectFirst("span.js-rmp-avg-rating")?.text()?.split(" ")?.last()?.toRatingInt()
+        val rating      = document.selectFirst("span.js-rmp-avg-rating")?.text()?.trim()?.toRatingInt()
 
         return newMovieLoadResponse(title, url, TvType.Movie, url) {
             this.posterUrl = poster
