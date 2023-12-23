3d22466b2a44e5b8bfbeaf8912cab087fdaf417e
keyiflerolsun <keyiflerolsun@gmail.com>
2023-12-23 03:14:56 +0300

    🕊
    🕊

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.0.4")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
@@ -77,7 +77,7 @@ subprojects {
         // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
         implementation(kotlin("stdlib"))                                              // Kotlin'in temel kütüphanesi
         implementation("com.github.Blatzar:NiceHttp:0.4.4")                           // HTTP kütüphanesi
-        implementation("org.jsoup:jsoup:1.16.2")                                      // HTML ayrıştırıcı
+        implementation("org.jsoup:jsoup:1.17.1")                                      // HTML ayrıştırıcı
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")   // Kotlin için Jackson JSON kütüphanesi
         implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")          // JSON-nesne dönüştürme kütüphanesi
     }
