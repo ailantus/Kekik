d7f6bbc37afe7d0db23b8923d8c1cd089e5c86a9
keyiflerolsun <keyiflerolsun@gmail.com>
2024-01-11 15:10:25 +0300

    🕊
    🕊

--- FilmMakinesi/src/main/kotlin/com/keyiflerolsun/FilmMakinesi.kt
+++ FilmMakinesi/src/main/kotlin/com/keyiflerolsun/FilmMakinesi.kt
@@ -19,6 +19,11 @@ class FilmMakinesi : MainAPI() {
     override val hasDownloadSupport   = true
     override val supportedTypes       = setOf(TvType.Movie)
 
+    // ! CloudFlare bypass
+    override var sequentialMainPage = true        // * https://recloudstream.github.io/dokka/-cloudstream/com.lagradost.cloudstream3/-main-a-p-i/index.html#-2049735995%2FProperties%2F101969414
+    // override var sequentialMainPageDelay       = 250L // ? 0.25 saniye
+    // override var sequentialMainPageScrollDelay = 250L // ? 0.25 saniye
+
     override val mainPage = mainPageOf(
         "${mainUrl}/page/"                                        to "Son Filmler",
         "${mainUrl}/film-izle/olmeden-izlenmesi-gerekenler/page/" to "Ölmeden İzle",
