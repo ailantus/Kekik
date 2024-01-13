b9c0d1b2fe86b1172efddb76ac63c705ce261fa9
Adem Ayaz <meademayaz@gmail.com>
2024-01-13 23:51:30 +0300

    ok

--- DiziKorea/src/main/kotlin/com/keyiflerolsun/DiziKoreaModels.kt
+++
@@ -1,9 +0,0 @@
-// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
-
-package com.keyiflerolsun
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class KoreaSearch(
-    @JsonProperty("theme") val theme: String
-)
