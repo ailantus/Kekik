d588fe9ae2149ec8feb6ccf0515b558fdea99996
keyiflerolsun <keyiflerolsun@gmail.com>
2024-02-07 14:58:17 +0300

    🕊
    🕊

--- HDFilmCehennemi/build.gradle.kts
+++ HDFilmCehennemi/build.gradle.kts
@@ -14,5 +14,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("Movie")
-    iconUrl = "https://www.google.com/s2/favicons?domain=www.hdfilmcehennemi.us&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=hdfilmcehennemi.com&sz=%size%"
 }
