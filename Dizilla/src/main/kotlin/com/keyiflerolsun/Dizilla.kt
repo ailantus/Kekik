36853b309aa9b96cec3651d689db454c33b056d1
keyiflerolsun <keyiflerolsun@gmail.com>
2024-05-18 16:46:05 +0300

    🕊
    🕊

--- Dizilla/src/main/kotlin/com/keyiflerolsun/Dizilla.kt
+++ Dizilla/src/main/kotlin/com/keyiflerolsun/Dizilla.kt
@@ -20,8 +20,8 @@ class Dizilla : MainAPI() {
 
     // ! CloudFlare bypass
     override var sequentialMainPage = true        // * https://recloudstream.github.io/dokka/-cloudstream/com.lagradost.cloudstream3/-main-a-p-i/index.html#-2049735995%2FProperties%2F101969414
-    // override var sequentialMainPageDelay       = 250L // ? 0.25 saniye
-    // override var sequentialMainPageScrollDelay = 250L // ? 0.25 saniye
+    override var sequentialMainPageDelay       = 250L // ? 0.25 saniye
+    override var sequentialMainPageScrollDelay = 250L // ? 0.25 saniye
 
     override val mainPage = mainPageOf(
         "${mainUrl}/tum-bolumler?page="    to "Altyazılı Bölümler",
