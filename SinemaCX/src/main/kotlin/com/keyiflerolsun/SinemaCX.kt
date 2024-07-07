0228375a6e64115ed09a1e769bf51037eea6fee4
keyiflerolsun <keyiflerolsun@gmail.com>
2024-07-07 11:06:48 +0300

    🕊
    🕊

--- SinemaCX/src/main/kotlin/com/keyiflerolsun/SinemaCX.kt
+++ SinemaCX/src/main/kotlin/com/keyiflerolsun/SinemaCX.kt
@@ -19,6 +19,11 @@ class SinemaCX : MainAPI() {
     override val hasDownloadSupport   = true
     override val supportedTypes       = setOf(TvType.Movie)
 
+    // ! CloudFlare bypass
+    override var sequentialMainPage = true        // * https://recloudstream.github.io/dokka/-cloudstream/com.lagradost.cloudstream3/-main-a-p-i/index.html#-2049735995%2FProperties%2F101969414
+    // override var sequentialMainPageDelay       = 250L // ? 0.25 saniye
+    // override var sequentialMainPageScrollDelay = 250L // ? 0.25 saniye
+
     override val mainPage = mainPageOf(
         "${mainUrl}/izle/aile-filmleri/page/"			 to		"Aile Filmleri",
         "${mainUrl}/izle/aksiyon-filmleri/page/"		 to		"Aksiyon Filmleri",
