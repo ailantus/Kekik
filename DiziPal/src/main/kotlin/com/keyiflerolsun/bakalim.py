14087fbf2225ae5c0e55560a316e3065673a741c
keyiflerolsun <keyiflerolsun@gmail.com>
2024-07-10 23:33:43 +0300

    🕊
    🕊

--- DiziPal/src/main/kotlin/com/keyiflerolsun/bakalim.py
+++ DiziPal/src/main/kotlin/com/keyiflerolsun/bakalim.py
@@ -7,7 +7,7 @@ from re           import findall
 
 oturum  = CloudScraper()
 
-mainUrl = "https://dizipal735.com"
+mainUrl = "https://dizipal737.com"
 pageUrl = f"{mainUrl}/diziler?kelime=&durum=&tur=1&type=&siralama="
 istek   = oturum.get(pageUrl)
 secici  = Selector(istek.text)
