03ccd8c97426852e643945f95a2fadbb6e6939ee
keyiflerolsun <keyiflerolsun@gmail.com>
2024-07-25 13:12:08 +0300

    🕊
    🕊

---
+++ SetFilmIzle/src/main/kotlin/com/keyiflerolsun/SetFilmIzle.kt
@@ -0,0 +1,124 @@
+// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+package com.keyiflerolsun
+
+import android.util.Log
+import org.jsoup.nodes.*
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.LoadResponse.Companion.*
+
+class SetFilmIzle : MainAPI() {
+    override var mainUrl              = "https://www.setfilmizle.lol"
+    override var name                 = "SetFilmIzle"
+    override val hasMainPage          = true
+    override var lang                 = "tr"
+    override val hasQuickSearch       = false
+    override val hasChromecastSupport = true
+    override val hasDownloadSupport   = true
+    override val supportedTypes       = setOf(TvType.Movie)
+
+    override val mainPage = mainPageOf(
+        "${mainUrl}/tur/aile/"        to "Aile",
+        "${mainUrl}/tur/aksiyon/"     to "Aksiyon",
+        "${mainUrl}/tur/animasyon/"   to "Animasyon",
+        "${mainUrl}/tur/belgesel/"    to "Belgesel",
+        "${mainUrl}/tur/bilim-kurgu/" to "Bilim-Kurgu",
+        "${mainUrl}/tur/biyografi/"   to "Biyografi",
+        "${mainUrl}/tur/dini/"        to "Dini",
+        "${mainUrl}/tur/dram/"        to "Dram",
+        "${mainUrl}/tur/fantastik/"   to "Fantastik",
+        "${mainUrl}/tur/genclik/"     to "Gençlik",
+        "${mainUrl}/tur/gerilim/"     to "Gerilim",
+        "${mainUrl}/tur/gizem/"       to "Gizem",
+        "${mainUrl}/tur/komedi/"      to "Komedi",
+        "${mainUrl}/tur/korku/"       to "Korku",
+        "${mainUrl}/tur/macera/"      to "Macera",
+        "${mainUrl}/tur/mini-dizi/"   to "Mini Dizi",
+        "${mainUrl}/tur/muzik/"       to "Müzik",
+        "${mainUrl}/tur/program/"     to "Program",
+        "${mainUrl}/tur/romantik/"    to "Romantik",
+        "${mainUrl}/tur/savas/"       to "Savaş",
+        "${mainUrl}/tur/spor/"        to "Spor",
+        "${mainUrl}/tur/suc/"         to "Suç",
+        "${mainUrl}/tur/tarih/"       to "Tarih",
+        "${mainUrl}/tur/western/"     to "Western"
+    )
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val document = app.get("${request.data}").document
+        val home     = document.select("div.items article").mapNotNull { it.toMainPageResult() }
+
+        return newHomePageResponse(request.name, home)
+    }
+
+    private fun Element.toMainPageResult(): SearchResponse? {
+        val title     = this.selectFirst("div.flbaslik")?.text() ?: return null
+        val href      = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
+        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("data-src"))
+
+        return newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = posterUrl }
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val document = app.get("${mainUrl}/?s=${query}").document
+
+        return document.select("div.result-item article").mapNotNull { it.toSearchResult() }
+    }
+
+    private fun Element.toSearchResult(): SearchResponse? {
+        val title     = this.selectFirst("div.title a")?.text() ?: return null
+        val href      = fixUrlNull(this.selectFirst("div.title a")?.attr("href")) ?: return null
+        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
+
+        return newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = posterUrl }
+    }
+
+    override suspend fun quickSearch(query: String): List<SearchResponse> = search(query)
+
+    override suspend fun load(url: String): LoadResponse? {
+        val document = app.get(url).document
+
+        val title           = document.selectFirst("h1")?.text()?.trim() ?: return null
+        val poster          = fixUrlNull(document.selectFirst("div.poster img")?.attr("src"))
+        val description     = document.selectFirst("div.wp-content p")?.text()?.trim()
+        val year            = document.selectFirst("div.extra span.C a")?.text()?.trim()?.toIntOrNull()
+        val tags            = document.select("div.sgeneros a").map { it.text() }
+        val rating          = document.selectFirst("span.dt_rating_vgs")?.text()?.trim()?.toRatingInt()
+        val duration        = document.selectFirst("span.runtime")?.text()?.split(" ")?.first()?.trim()?.toIntOrNull()
+        val recommendations = document.select("div.srelacionados article").mapNotNull { it.toLoadResult() }
+        val actors          = document.select("span.valor a").map { Actor(it.text()) }
+        val trailer         = Regex("""embed\/(.*)\?rel""").find(document.html())?.groupValues?.get(1)?.let { "https://www.youtube.com/embed/$it" }
+
+        return newMovieLoadResponse(title, url, TvType.Movie, url) {
+            this.posterUrl       = poster
+            this.plot            = description
+            this.year            = year
+            this.tags            = tags
+            this.rating          = rating
+            this.rating          = rating
+            this.duration        = duration
+            this.recommendations = recommendations
+            addActors(actors)
+            addTrailer(trailer)
+        }
+    }
+
+    private fun Element.toLoadResult(): SearchResponse? {
+        val title     = this.selectFirst("a img")?.attr("alt") ?: return null
+        val href      = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
+        val posterUrl = fixUrlNull(this.selectFirst("a img")?.attr("data-src"))
+
+        return newMovieSearchResponse(title, href, TvType.Movie) { this.posterUrl = posterUrl }
+    }
+
+    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
+        Log.d("STF", "data » ${data}")
+        val document = app.get(data).document
+
+        // TODO:
+        // loadExtractor(iframe, "${mainUrl}/", subtitleCallback, callback)
+
+        return true
+    }
+}
