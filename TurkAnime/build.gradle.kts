a71088c03d8183753db83605e6b39d28ccb2d3ba
keyiflerolsun <keyiflerolsun@gmail.com>
2024-07-29 09:40:51 +0300

    🕊
    🕊

---
+++ TurkAnime/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 0
+
+cloudstream {
+    authors     = listOf("keyiflerolsun")
+    language    = "tr"
+    description = "!!! YAPIM AŞAMASINDA !!!"
+    // description = "Türk Anime TV - Türkiye'nin Online Anime izleme sitesi."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 3 // will be 3 if unspecified
+    tvTypes = listOf("Anime")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.turkanime.co&sz=%size%"
+}
