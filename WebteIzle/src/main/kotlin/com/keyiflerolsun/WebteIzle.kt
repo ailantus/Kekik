8c3a31c86240da091283e06009d67f8a360a255d
keyiflerolsun <keyiflerolsun@gmail.com>
2024-08-06 15:59:48 +0300

    🕊 #7
    🕊

--- WebteIzle/src/main/kotlin/com/keyiflerolsun/WebteIzle.kt
+++ WebteIzle/src/main/kotlin/com/keyiflerolsun/WebteIzle.kt
@@ -201,15 +201,9 @@ class WebteIzle : MainAPI() {
 
                     continue
                 } else if (iframe.contains("playerjs-three.vercel.app")) {
-                    val iSource = app.get(iframe, referer="${mainUrl}/").text
-
-                    val extractedValue = Regex("""file":"(.*)",""").find(iSource)?.groupValues?.get(1)?.replace("\\\\x", "")
-                    val decoded        = extractedValue?.let {
+                    val decoded = iframe.substringAfter("&v=")?.let {
                         val hexString = it.replace("\\x", "")
-
-                        val bytes = hexString.chunked(2).map { chunk ->
-                            chunk.toInt(16).toByte()
-                        }.toByteArray()
+                        val bytes     = hexString.chunked(2).map { chunk -> chunk.toInt(16).toByte() }.toByteArray()
 
                         bytes.toString(Charsets.UTF_8)
                     } ?: ""
