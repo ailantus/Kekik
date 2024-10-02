429177a62f1d425096f440ad6635b2f7e65874fc
keyiflerolsun <keyiflerolsun@users.noreply.github.com>
2024-10-02 21:02:05 +0000

    ♻️ Domain Değişikliği
    🔄 Otomatik domain güncellemeleri yapıldı.

--- UgurFilm/src/main/kotlin/com/keyiflerolsun/UgurFilm.kt
+++ UgurFilm/src/main/kotlin/com/keyiflerolsun/UgurFilm.kt
@@ -9,7 +9,7 @@ import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 
 class UgurFilm : MainAPI() {
-    override var mainUrl              = "https://ugurfilm7.com"
+    override var mainUrl              = "https://ugurfilm7.com/?doing_wp_cron=1727902919.3723928928375244140625"
     override var name                 = "UgurFilm"
     override val hasMainPage          = true
     override var lang                 = "tr"
